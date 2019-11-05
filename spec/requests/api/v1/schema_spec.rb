c30add91fb631ba0da049f3a4c93c1790ec79b35
Brad Smith <brad@smithwebtek.com>
2019-11-05 10:07:38 -0500

    WIP 191105-1007a

--- spec/requests/api/v1/schema_spec.rb
+++ spec/requests/api/v1/schema_spec.rb
@@ -4,37 +4,37 @@ describe "Validate planning schema" do
     it "matches" do
       get '/planning'
 
-      expect(response).to match_response_schema("release")
+      expect(response).to match_response_schema("release1_1")
     end
 
     it "The file contains 1 release describing 1 contracting process" do
       get '/planning'
 
-      expect(response).to match_response_schema("release")
+      expect(response).to match_response_schema("release1_1")
     end
 
     it "The schema version specified in the file is 1.1." do
       get '/planning'
 
-      expect(response).to match_response_schema("release")
+      expect(response).to match_response_schema("release1_1")
     end
 
     # it "The publisher named in the file is Open Data Services Co-operative Limited." do
     #   get '/planning'
 
-    #   expect(response).to match_response_schema("release")
+    #   expect(response).to match_response_schema("release1_1")
     # end
 
     # it "The license is http://opendatacommons.org/licenses/pddl/1.0/." do
     #   get '/planning'
 
-    #   expect(response).to match_response_schema("release")
+    #   expect(response).to match_response_schema("release1_1")
     # end
 
     # it "Publication policy is https://github.com/open-contracting/sample-data/." do
     #   get '/planning'
 
-    #   expect(response).to match_response_schema("release")
+    #   expect(response).to match_response_schema("release1_1")
     # end
   end
 end
